d5040b6a11727e5d2fb9180e4544300cf5d29f6c
mattgo327 <99551057+mattgo327@users.noreply.github.com>
2022-11-18 18:56:24 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    return [
+        'url' => 'http://localhost/leccion37/'
+    ];
+    // vamos retornar un array y ese lo vamos a guardar en una variable
+    // y desde esa varible usamos las posicion 'url'
+    // eso es gracias a que es un array asociativo
+    //entiendo
