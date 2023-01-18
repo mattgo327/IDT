4ff9900fd274b271cdc968eed5fd80b104ed5695
Matias Garay <matiasgaray104@gmail.com>
2023-01-18 09:50:18 -0300

    perfil

--- perfil.php
+++ perfil.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(isset($_SESSION["acceso"]) && $_SESSION["acceso"]== "true"){
-        echo "Hola {$_SESSION['nombre']}, como estas?";
+        echo "Hola {$_SESSION['nombre']}, como estas? ";
         echo "Deseas cerrar sesion? <a href='./php/CerrarSesion.php'>Salir</a>";
     }else{
         echo "Hola, debes iniciar sesion!";
